a2ed0323bccf2fa9171522c97ec230de86480d41
Utsav Baral <manutsssav@gmail.com>
2019-08-05 14:12:59 +0545

    solutions add

--- index.js
+++ index.js
@@ -201,3 +201,30 @@ function accum(s) {
     }
     return arr.join('-')
 }
+
+
+//likes(([]), 'no one likes this');
+//likes((['Peter']), 'Peter likes this');
+//likes((['Jacob', 'Alex']), 'Jacob and Alex like this');
+//likes((['Max', 'John', 'Mark']), 'Max, John and Mark like this');
+//likes((['Alex', 'Jacob', 'Mark', 'Max']), 'Alex, Jacob and 2 others like this');
+function likes(names) {
+    if(names.length == 0){
+        return 'no one likes this'
+    }
+    for (var i = 0; i < names.length; i++) {
+        if(names.length == 1){
+            return names[0] + ' likes this'
+        }
+        else if(names.length == 2){
+            return names[0] + ' and ' +names[1] + ' like this'
+        }
+       else if(names.length == 3 ) {
+        return names[0] + ', ' +names[1] + ' and ' + names[2] + ' like this'
+       }
+       else {
+        count = names.length - 2
+        return names[0] + ', ' +names[1] + ' and ' + count + ' others like this'
+       }
+    }
+}
